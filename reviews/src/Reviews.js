f5f4aa5164329648e25fc2692516027d3b56b8ae
Cynthia Nosiri <cynthiasamuels98@gmail.com>
2021-09-16 21:30:44 -0400

    reviewsapp

---
+++ reviews/src/Reviews.js
@@ -0,0 +1,47 @@
+import React,{useState} from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Reviews = () => {
+    const [index, setindex] = useState(0);
+    const {name, job, image, text} = people[index]
+
+    function handlenext(){
+        setindex(index+1)
+    }
+    function handleprev(){
+        setindex(index-1)
+    }
+    function handlenextdisable(){
+        if(index === people.length - 1){
+            return true
+        }
+  
+    function handleprevdisable(){
+        if(index === 0){
+            return true
+        }
+    }
+    return (
+        
+      <article className="review">
+          <div className="img-container">
+              <img src={image} alt={name} className="person-img"/>
+              <span className="quote-icon">
+                <FaQuoteRight/>
+              </span>
+          </div>
+
+          <h4 className="author">{name}</h4>
+          <p className="job">{job}</p>
+          <p className="info">{text}</p>
+
+          <div>
+          <button disabled={handleprevdisable()} className="prev-btn" onClick={(()=>handleprev())}><FaChevronLeft/></button>
+          <button disabled={handlenextdisable()} className="next-btn" onClick={(()=>handlenext())}><FaChevronRight/></button>
+          </div>
+      </article>  
+    )
+}
+
+export default Reviews;
